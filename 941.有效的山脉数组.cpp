fcbfac7aaeb39558cbf5ca43ecd7ac55255ca834
Akko <33564554+LvFinch@users.noreply.github.com>
2020-11-04 14:35:57 +0800

    Update 941.有效的山脉数组.cpp

--- 941.有效的山脉数组.cpp
+++ 941.有效的山脉数组.cpp
@@ -10,6 +10,10 @@ class Solution
 public:
     bool validMountainArray(vector<int> &A)
     {
+        // Solution 1
+        // 60ms(>81.85%)
+        // 21.2MB(>5.27%)
+        /*
         // int N = A.length(); //C++用size判断
         int N = A.size();
         int i = 0;
@@ -29,6 +33,32 @@ public:
             i++;
         }
         return i == N - 1;
+        */
+
+        //Solution 2
+        //
+        //
+
+        int N = A.size();
+        int left = 0;
+        int right = N - 1;
+
+        while (left + 1 < N && A[left] < A[left + 1])
+        {
+            left++;
+        }
+        if (left == 0 || left == N - 1)
+        {
+            return false;
+        }
+        else
+        {
+            while (right > left || A[right] > A[right - 1])
+            {
+                right--;
+            }
+        }
+        return left == right && A[left] != A[right];
     }
 };
 // @lc code=end
